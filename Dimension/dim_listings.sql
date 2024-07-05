d1dff741f0528efed7efd1ec72d54101f8248a8f
Dilip Kumar S <dilip1100@gmail.com>
2024-07-05 13:20:24 +0530

    Create dim_listings.sql

---
+++ Dimension/dim_listings.sql
@@ -0,0 +1,25 @@
+WITH raw_listings AS (
+    SELECT
+        id,
+        name,
+        room_type,
+        CASE
+            WHEN minimum_nights = 0 THEN 1
+            ELSE minimum_nights
+        END AS minimum_nights,
+        host_id,
+        REPLACE(price, '$', '')::NUMBER(10, 2) AS price,
+        created_at,
+        updated_at
+    FROM {{ source('raw', 'raw_lst') }}
+)
+SELECT
+    id AS listing_id,
+    name AS listing_name,
+    room_type,
+    minimum_nights,
+    host_id,
+    price,
+    created_at,
+    updated_at
+FROM raw_listings
